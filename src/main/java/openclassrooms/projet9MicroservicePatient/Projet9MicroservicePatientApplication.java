c944b7b745f858830acd5e062ace208532803440
Mourad-x64 <95309482+Mourad-x64@users.noreply.github.com>
2024-04-21 17:56:40 +0200

    initial commit

---
+++ src/main/java/openclassrooms/projet9MicroservicePatient/Projet9MicroservicePatientApplication.java
@@ -0,0 +1,13 @@
+package openclassrooms.projet9MicroservicePatient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projet9MicroservicePatientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projet9MicroservicePatientApplication.class, args);
+	}
+
+}
