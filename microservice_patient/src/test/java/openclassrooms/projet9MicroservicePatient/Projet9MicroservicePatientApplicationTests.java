e6c57127a3c9fe3887e959bff46b45f9580fbb78
Mourad-x64 <95309482+Mourad-x64@users.noreply.github.com>
2024-04-23 18:43:40 +0200

    initial commit

---
+++ microservice_patient/src/test/java/openclassrooms/projet9MicroservicePatient/Projet9MicroservicePatientApplicationTests.java
@@ -0,0 +1,13 @@
+package openclassrooms.projet9MicroservicePatient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projet9MicroservicePatientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
