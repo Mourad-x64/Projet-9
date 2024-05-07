d1e5d19bba1afee26dea735a5fc423ba336d0d60
Mourad-x64 <95309482+Mourad-x64@users.noreply.github.com>
2024-05-07 13:44:48 +0200

    initial commit

---
+++ microservice_gateway/src/main/java/com/openclassrooms/projet9microservicegateway/Projet9microservicegatewayApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.projet9microservicegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projet9microservicegatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projet9microservicegatewayApplication.class, args);
+	}
+
+}
